39e2f2b32f2abf5af1bcec87c20ba0a678871eba
nmak1 <nmak1@rambler.ru>
2023-10-03 19:08:35 +0300

    change sum function

--- app/src/main/java/ru/netology/yandexmaps/adapter/MyListAdapter.kt
+++ app/src/main/java/ru/netology/yandexmaps/adapter/MyListAdapter.kt
@@ -1,5 +1,6 @@
 package ru.netology.yandexmaps.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -11,13 +12,14 @@ import ru.netology.yandexmaps.model.Model
 
 class MyListAdapter(var mCtx: Context, var resource: Int, var items: List<Model>) :
     ArrayAdapter<Model>(mCtx, resource, items) {
+    @SuppressLint("ViewHolder")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val LayoutInflater: LayoutInflater = LayoutInflater.from(mCtx)
         val view: View = LayoutInflater.inflate(resource, null)
         val textView: TextView = view.findViewById(R.id.idtext)
         val lat: TextView = view.findViewById(R.id.lat)
         val lon: TextView = view.findViewById(R.id.lon)
-        var mItems: Model = items[position]
+        val mItems: Model = items[position]
         textView.text = mItems.title
         lat.text = mItems.lat
         lon.text = mItems.lon
